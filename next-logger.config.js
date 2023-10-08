6e976508c58684dd837f004fd93f9a6a6d05fb67
bennyhsieh <hotman0901@gmail.com>
2023-10-08 23:55:14 +0800

    feat: add pino logger

---
+++ next-logger.config.js
@@ -0,0 +1,12 @@
+// next-logger.config.js
+const pino = require('pino')
+
+const logger = defaultConfig =>
+  pino({
+    ...defaultConfig,
+    mixin: () => ({ name: 'pino-logger' }),
+  })
+
+module.exports = {
+  logger,
+}
