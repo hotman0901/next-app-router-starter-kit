4463cd3eb3c9a7b1becf9c323b02555eee13eac2
bennyhsieh <benny.hsieh@jyintgroup.com>
2023-10-31 17:12:37 +0800

    feat: 修復升級 tanstack v5 產生的錯誤

--- app/[locale]/hydration/page.tsx
+++ app/[locale]/hydration/page.tsx
@@ -1,13 +1,13 @@
-import { dehydrate } from "@tanstack/query-core";
+import { dehydrate } from '@tanstack/query-core';
 
-import getQueryClient from "@/utils/getQueryClient";
-import Hydrate from "@/utils/hydrate.client";
+import getQueryClient from '@/utils/getQueryClient';
+import Hydrate from '@/utils/hydrate.client';
 
-import ListUsers from "./list-users";
-import { User } from "./types";
+import ListUsers from './list-users';
+import { User } from './types';
 
 async function getUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = (await res.json()) as User[];
   return users;
 }
@@ -15,7 +15,7 @@ async function getUsers() {
 // 這個做法比較推薦
 export default async function Hydation() {
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(["users"], getUsers);
+  await queryClient.prefetchQuery({ queryKey: ['users'], queryFn: getUsers });
   const dehydratedState = dehydrate(queryClient);
 
   return (
