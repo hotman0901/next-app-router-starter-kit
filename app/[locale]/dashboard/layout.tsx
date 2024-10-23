82400d6b7191a0e4f1a9c5b946a2280275c42d9b
bennyhsieh <benny.hsieh@jyintgroup.com>
2024-10-23 14:20:30 +0800

    feat: 更新 eslint 報錯問題

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,23 +1,19 @@
-'use client'
+'use client';
+import { ReactNode } from 'react';
 
-import { ReactElement } from 'react'
+import { I18nProviderClient } from '@/locales/client';
 
-import { I18nProviderClient } from '@/locales/client'
-
-// import en from '@/locales/en';
-
-
-export default function SubLayout({
-  children,
-  params: { locale = 'en' },
-}: {
-  children: ReactElement
-  params: { locale: string }
+export default function SubLayout(o: {
+  children: ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  params: any;
 }) {
+  const { children, params } = o;
+  const ln = params?.locale || 'en';
   return (
-    <I18nProviderClient locale={locale}>
+    <I18nProviderClient locale={ln}>
       <div>123123</div>
       {children}
     </I18nProviderClient>
-  )
+  );
 }
--- app/[locale]/lan-server/layout.tsx
+++ app/[locale]/lan-server/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactElement } from 'react';
 
-export default function SubLayout({
-  children,
-}: {
+export default function SubLayout(o: {
   children: ReactElement;
-  params: { locale: string };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  params: Promise<{ locale: any }>;
 }) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { children, params } = o;
   return <div>{children}</div>;
 }
