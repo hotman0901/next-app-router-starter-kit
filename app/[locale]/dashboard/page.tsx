8bf0aefb2c449a75f905df16dfefc802b65f5172
bennyhsieh <benny.hsieh@jyintgroup.com>
2026-03-02 15:41:13 +0800

    Updates package dependencies
    Bumps various direct and dev dependencies to their latest minor and patch versions.
    This provides improved stability, bug fixes, and access to new features from libraries such as Rive, Tanstack Query, Biome, Tailwind CSS, and Axios.
    Also updates Biome configuration, including the schema and linter rules, to align with the new version and enhance code quality.

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,23 +1,22 @@
-"use client";
-import { useRouter } from "next/navigation";
+'use client';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useI18n, useScopedI18n } from '@/locales/client';
 
-import { useI18n, useScopedI18n } from "@/locales/client";
-import Link from "next/link";
-
-export default function page() {
+export default function Page() {
   const router = useRouter();
   const t = useI18n();
-  const helloT = useScopedI18n("hello");
-  const nameT = useScopedI18n("hello.world");
+  const helloT = useScopedI18n('hello');
+  const nameT = useScopedI18n('hello.world');
 
   return (
     <div>
-      <Link href="/stream">stream page</Link>
-      <p>{t("hello")}</p>
-      <p>{t("hello.world.welcome", { name: "benny" })}</p>
-      <p>{helloT("world")}</p>
-      <p>{nameT("welcome", { name: "john" })}</p>
-      <button onClick={() => router.push("/")}>go to index</button>
+      <Link href='/stream'>stream page</Link>
+      <p>{t('hello')}</p>
+      <p>{t('hello.world.welcome', { name: 'benny' })}</p>
+      <p>{helloT('world')}</p>
+      <p>{nameT('welcome', { name: 'john' })}</p>
+      <button onClick={() => router.push('/')}>go to index</button>
     </div>
   );
 }
